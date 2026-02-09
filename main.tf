314015de7d7e6e612571e3584b4a64d83d0caa61
John Borwick <borwick@uw.edu>
2026-02-09 07:23:12 -0800

    Add s3:HeadObject to ECS task role policy

--- main.tf
+++ main.tf
@@ -373,6 +373,7 @@ resource "aws_iam_role_policy" "ecs_task_s3" {
       Effect = "Allow"
       Action = [
         "s3:GetObject",
+        "s3:HeadObject",
         "s3:PutObject",
         "s3:DeleteObject",
         "s3:ListBucket",
